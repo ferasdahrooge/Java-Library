f8660fe82f1446e7420b990cb771f0163f9a7d13
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-03-11 01:31:31 -0700

    Implement insert sorted method

--- demo/src/main/java/com/example/DataStructures/Linear/SingularLinkedList.java
+++ demo/src/main/java/com/example/DataStructures/Linear/SingularLinkedList.java
@@ -114,4 +114,47 @@ public class SingularLinkedList{
             current = current.next;
         }
     }
+
+    public void sortedInsert(SNode node){
+        if (head == null){
+            head = node;
+            tail = node;
+            size++;
+            return;
+        }
+
+        
+        if(!isItSorted()){
+            sort();
+        }
+        SNode current = head;
+        if (current.data > node.data){
+            this.insertHead(node);
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (current.data == node.data){
+                SNode temp = current.next;
+                node.next = temp;
+                current.next = node;
+                size++;
+                return;
+            }
+
+            if (i == size - 1) {
+                this.insertTail(node);
+                return;
+            }
+
+            if (current.next.data > node.data){
+                node.next = current.next;
+                current.next = node;
+                size++;
+                return;
+            }
+            current = current.next;
+        }
+        
+    }
 }
