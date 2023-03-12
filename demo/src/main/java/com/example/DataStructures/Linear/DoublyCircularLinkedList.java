edb20d8887f29a6ce2894cf8bd5c4ca5bb2f1f6f
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-03-11 17:07:24 -0700

    Define Constructors

--- demo/src/main/java/com/example/DataStructures/Linear/DoublyCircularLinkedList.java
+++ demo/src/main/java/com/example/DataStructures/Linear/DoublyCircularLinkedList.java
@@ -2,6 +2,18 @@ package com.example.dataStructures.Linear;
 
 import com.example.dataStructures.nodes.DNode;
 
-public class DoublyCircularLinkedList {
-    
+public class DoublyCircularLinkedList extends DoublyLinkedList{
+    public DoublyCircularLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public DoublyCircularLinkedList(DNode node) {
+        this.head = node;
+        this.tail = node;
+        node.next = node;
+        node.previous = node;
+        this.size = 1;
+    }
 }
