f44cc8858d906e9d3c318312d2dad2f792cd95fd
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-03-11 17:21:59 -0700

    Add Queue . Add Enqueue

---
+++ demo/src/main/java/com/example/DataStructures/Linear/QueueLinkedList.java
@@ -0,0 +1,11 @@
+package com.example.DataStructures.Linear;
+
+import com.example.DataStructures.nodes.SNode;
+
+public class Queuell extends SLL{
+
+    public void enqueue(SNode node){
+        super.insertTail(node);
+    }
+
+}
