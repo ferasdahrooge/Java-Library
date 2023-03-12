7f110502553af0a57d4a5aa7c93ca1e876c7b8e5
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-03-11 17:20:20 -0700

    Build test Stack

---
+++ demo/src/main/java/com/example/DataStructures/Linear/StackLinkedListTest.java
@@ -0,0 +1,26 @@
+package com.example.DataStructures.Linear;
+
+import com.example.DataStructures.nodes.SNode;
+
+public class StackLinkedListTest {
+    public static void main(String[] args) {
+        Stackll list = new Stackll();
+        SNode node1 = new SNode(1);
+        SNode node2 = new SNode(2);
+        SNode node3 = new SNode(3);
+        SNode node4 = new SNode(4);
+        SNode node5 = new SNode(5);
+        SNode node6 = new SNode(6);
+        SNode node7 = new SNode(7);
+
+        list.push(node1);
+        list.push(node2);
+        list.push(node3);
+        list.isEmpty();
+        list.pop();
+        list.peek();
+        list.print();
+
+    }
+    
+}
