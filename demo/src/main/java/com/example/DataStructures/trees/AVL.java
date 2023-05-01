556a4f2aec8d886c7996477a7878ec7f9617388c
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-05-01 12:47:42 -0600

    AVL constructor

--- demo/src/main/java/com/example/DataStructures/trees/AVL.java
+++ demo/src/main/java/com/example/DataStructures/trees/AVL.java
@@ -4,4 +4,21 @@ import com.example.DataStructures.nodes.TNode;
 
 public class AVL extends BST{
   
+  private TNode root;
+
+  public AVL(){
+      this.root = null;
+  }
+
+  public AVL(int val){
+      TNode node = new TNode(val,0,null,null,null);
+      root = node;
+  }
+
+  public AVL(TNode node){
+      this.root = node;
+      if (node.getLeft() != null) insert(node.getLeft());
+      if (node.getRight() != null) insert(node.getRight());
+      
+  }
 }
