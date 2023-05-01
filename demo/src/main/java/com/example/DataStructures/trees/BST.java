e02d4695614ab0d26390dfc2434d84d67d7afb5c
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-05-01 12:36:52 -0600

    insert based on the value

--- demo/src/main/java/com/example/DataStructures/trees/BST.java
+++ demo/src/main/java/com/example/DataStructures/trees/BST.java
@@ -32,4 +32,27 @@ public class BST {
     return this.root = node;
   }
 
+  public void insert(int val){
+    TNode node = new TNode(val,0,null,null,null);
+    TNode traversedNode = null;
+    TNode top = this.root;
+
+    while(top != null){
+        traversedNode = top;
+        if(node.getData() < top.getData()){
+            top = top.getLeft();
+        } else{
+            top = top.getRight();
+        }
+    }
+
+    node.setParent(traversedNode);
+    if (traversedNode == null){
+        root = node;
+    } else if (node.getData() < traversedNode.getData()){
+        traversedNode.setLeft(node);
+    } else{
+        traversedNode.setRight(node);
+    }
+}
 }
